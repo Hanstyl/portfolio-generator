b2984f89a938f104d4de97759a2f945ee6ae8717
Nathan Nelson <nathan.nelson042@gmail.com>
2021-11-17 23:20:24 -0700

    added ability to capture. Ready to merge to dev

---
+++ app.js
@@ -0,0 +1,24 @@
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+
+
+
+
+
+
+
+
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('================');
+
+    // Is the same as
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+    
+};
+
+printProfileData(profileDataArgs);
