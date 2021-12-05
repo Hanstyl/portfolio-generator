eda9aaa6cde433dec9b2b91ce7486f21c23d9e06
Nathan Nelson <nathan.nelson042@gmail.com>
2021-12-04 23:18:08 -0700

    created the setup that takes user input and reated a basic html file

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-// const profileDataArgs = process.argv.slice(2, process.argv.length); ----directions said to comment or delete previous lesson code.
+const fs = require('fs');
+
+const generatePage = require('./src/page-template.js');
+
+const profileDataArgs = process.argv.slice(2);
+
+const [name, github] = profileDataArgs;
 
 
-const generatePage = () => 'Name: Nathan, Github: nathanhub';
-console.log(generatePage());
+
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
+
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
